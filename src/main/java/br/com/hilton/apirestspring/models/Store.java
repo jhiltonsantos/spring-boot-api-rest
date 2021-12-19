612c233a6e8b83ad760c38e70d3346c1f64cf9ea
Hilton Santos <jhilton930@gmail.com>
2021-12-19 19:09:59 -0300

    Add StoreModel

--- src/main/java/br/com/hilton/apirestspring/models/Store.java
+++ src/main/java/br/com/hilton/apirestspring/models/Store.java
@@ -1,2 +1,17 @@
-package br.com.hilton.apirestspring.models;public class Store {
+package br.com.hilton.apirestspring.models;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="TB_STORE")
+public class Store implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String description;
+
 }
