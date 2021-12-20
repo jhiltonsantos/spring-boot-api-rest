c13b97e7d684c42952bb718a068de34e10d1b2a7
Hilton Santos <jhilton930@gmail.com>
2021-12-20 02:05:49 -0300

    Update Swagger

--- src/main/java/br/com/hilton/apirestspring/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/hilton/apirestspring/config/swagger/SwaggerConfigurations.java
@@ -1,12 +1,17 @@
 package br.com.hilton.apirestspring.config.swagger;
 
 import br.com.hilton.apirestspring.models.User;
+import io.swagger.models.Model;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfigurations {
@@ -18,6 +23,15 @@ public class SwaggerConfigurations {
                 .apis(RequestHandlerSelectors.basePackage("br.com.hilton.apirestspring"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(User.class);
+                .ignoredParameterTypes(User.class)
+                .globalOperationParameters(Arrays.asList(
+                        new ParameterBuilder()
+                                .name("Authorization")
+                                .description("Header token JWT")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build()
+                ));
     }
 }
